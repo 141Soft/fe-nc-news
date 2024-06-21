417f326e9ba2a6798f2181f35257dadccec447c3
141Soft <141Soft@proton.me>
2024-06-21 11:13:40 +0100

    Implements sorting of article request response and url updating

--- src/components/Article/ArticleSort.jsx
+++ src/components/Article/ArticleSort.jsx
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react"
 
 
-function ArticleSort() {
-    const [sortType, setSortType] = useState('')
+function ArticleSort({ searchParams, setSearchParams }) {
+
+    const handleChange = (event) => {
+        const newParams = new URLSearchParams(searchParams);
+        newParams.set(event.target.name, event.target.value);
+        setSearchParams(newParams);
+    } 
 
 
     return (
-        <div>
-            <select>
-                <option>
-                    Sort-by:
-                </option>
+        <div className="articleSort">
+            <label>Sort By:
+            <select name="sort" onChange = {handleChange}>
+                <option value={'created_at'}>Date</option>
+                <option value={'comment_count'}>Comments</option>
+                <option value={'votes'}>Votes</option>
             </select>
+            </label>
+            <label>Order:
+            <select name="order" onChange = {handleChange}>
+                <option value={'asc'}>Ascending</option>
+                <option value={'desc'}>Descending</option>
+            </select>
+            </label>
         </div>
     )
 }
+
+export default ArticleSort;
--- src/components/Article/ArticlesList.jsx
+++ src/components/Article/ArticlesList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { getArticles } from '../../api';
 import ArticleStub from './ArticleStub';
+import ArticleSort from './ArticleSort';
 
 function ArticlesList({ articles, setArticles }) {
 
@@ -11,6 +12,13 @@ function ArticlesList({ articles, setArticles }) {
     useEffect(()=> {
         getArticles(searchParams.get("topic"))
         .then((fetchedArticles) => {
+            if(searchParams.get("sort")){
+                const sortBy = searchParams.get("sort")
+                searchParams.get("order") === 'desc'
+                ? fetchedArticles.sort((a,b) => { return a[sortBy] - b[sortBy]})
+                : fetchedArticles.sort((a,b) => { return b[sortBy] - a[sortBy]})
+            }
+            
             setArticles(fetchedArticles);
             setIsLoading(false);
         })
@@ -19,6 +27,7 @@ function ArticlesList({ articles, setArticles }) {
     if(!isLoading)
     return (
         <div>
+            <ArticleSort searchParams={searchParams} setSearchParams={setSearchParams}/>
             <div className='articleBox'>
                 <ul className = "articlesList">
                     {articles.map((article) => {
